dfbd2831aadfb0ba0be740866338cf37d802efe5
Jordan Rhode <2262659+JordanRhode@users.noreply.github.com>
2025-09-05 07:53:54 -0500

    Lint cleanup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,15 +10,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     ignores: [
+      'lib/optimizely/types/generated.ts',
       'node_modules/**',
       '.next/**',
       'out/**',
       'build/**',
       'next-env.d.ts',
     ],
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
